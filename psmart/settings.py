3329cafc0b814caef2d32d0cda1b4466de5f42b0
Domodan <ongomdaniel9@gmail.com>
2022-06-01 15:07:48 +0300

    minor change

--- psmart/settings.py
+++ psmart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9-0wlun7tn-!un#t%ts-s#t__^p#q7@rqit^1u_wmj%dgo)m^l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '10.0.2.2', '127.0.0.1', ]
+ALLOWED_HOSTS = [ '10.0.2.2', '127.0.0.1', '196.43.134.157', ]
 
 
 # Application definition
